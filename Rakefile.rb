0aa19a6f5bd58cefbb8087ff828fa52fe1f2c25f
hgarcia <hernan@dynamicprogrammer.com>
2014-09-23 17:57:30 -0400

    Build

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,17 +8,10 @@ namespace :build do
 	desc 'Pre building the site for local testing'
 	task :pre => [:merge_and_minimize_css] do
 		sh 'jekyll build'
-<<<<<<< HEAD
-		sh 'cp config.ru ../heroku_photos'
-		sh 'cp Procfile ../heroku_photos'
-		sh 'cp Gemfile ../heroku_photos'
-    	sh 'cp robots.txt ../heroku_photos'
-=======
 		sh 'cp config.ru ../heroku_outdoors'
 		sh 'cp Procfile ../heroku_outdoors'
 		sh 'cp Gemfile ../heroku_outdoors'
-    	sh 'cp robots.txt ../heroku_outdoors'
->>>>>>> Build
+    sh 'cp robots.txt ../heroku_outdoors'
 	end
 
 	desc 'Minimizing and combining css files'
@@ -38,35 +31,18 @@ namespace :build do
 
 	desc 'Generates the static files'
 	task :jekyll => [:github] do
-<<<<<<< HEAD
-		chdir '../heroku_photos'
-		sh 'git pull heroku master' do |ok, res|
-		end
-		chdir '../photos'
-=======
 		chdir '../heroku_outdoors'
 		sh 'git pull heroku master' do |ok, res|
 		end
 		chdir '../outdoors'
->>>>>>> Build
 		sh 'jekyll build'
 	end
 
 	desc 'Moves to the heroku folder'
 	task :deploy => [:jekyll] do
-<<<<<<< HEAD
-		sh 'cp config.ru ../heroku_photos'
-		sh 'cp Procfile ../heroku_photos'
-		sh 'cp Gemfile ../heroku_photos'
-    	sh 'cp robots.txt ../heroku_photos'
-		chdir '../heroku_photos'
-=======
-		sh 'cp config.ru ../heroku_outdoors'
-		sh 'cp Procfile ../heroku_outdoors'
 		sh 'cp Gemfile ../heroku_outdoors'
     	sh 'cp robots.txt ../heroku_outdoors'
 		chdir '../heroku_outdoors'
->>>>>>> Build
 		sh 'git pull heroku master' do |ok, res|
 		end
 		sh 'bundle install'
